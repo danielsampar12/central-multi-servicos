6477ae88657d6c257b53826d1274eebce6fae562
danielsampar <daniel.sampar12@gmail.com>
2023-09-04 18:28:27 -0300

    fix: intro section responsiveness for smaller screens

--- src/components/Buttons/IntroButton/index.tsx
+++ src/components/Buttons/IntroButton/index.tsx
@@ -7,9 +7,9 @@ export function IntroButton() {
     <button
       onClick={() => navigateToElementId('#footer')}
       type="button"
-      className="ContactUsButton w-full h-12 rounded-3xl px-10 py-4 gap-2 inline-flex items-center justify-center bg-gradient-to-r from-0.17% from-cms-green via-62.04% via-cms-light-blue to-100% to-cms-blue"
+      className="ContactUsButton w-full reg:h-12 h-10 rounded-3xl py-4 gap-2 inline-flex items-center justify-center bg-gradient-to-r from-0.17% from-cms-green via-62.04% via-cms-light-blue to-100% to-cms-blue"
     >
-      <p className="ContactUs text-white text-xl font-medium tracking-wide">
+      <p className="ContactUs text-white reg:text-xl text-base font-medium tracking-wide">
         Fale conosco
       </p>
     </button>
--- src/components/Sections/IntroSection/index.tsx
+++ src/components/Sections/IntroSection/index.tsx
@@ -6,25 +6,25 @@ import { IntroButton } from '@/components/Buttons/IntroButton';
 
 export function IntroSection() {
   return (
-    <div className="Container flex flex-row justify-start py-24 px-28">
-      <div className="InfoContainer flex flex-col justify-center items-start">
-        <div className="TextContainer flex flex-col justify-center items-start w-[42rem] h-60">
+    <div className="Container reg:flex reg:flex-row reg:justify-start reg:py-24 reg:px-28 regmax:grid regmax:grid-cols-5 py-12 px-16">
+      <div className="InfoContainer col-span-3 flex flex-col justify-center items-start">
+        <div className="TextContainer flex flex-col justify-center items-start reg:w-[42rem] reg:h-60 h-40">
           <BigGradientText text="Crédito" />
-          <span className="text-black text-[5rem] leading-none font-normal tracking-spaced">
+          <span className="text-black reg:text-[5rem] text-[3.5rem] leading-none font-normal reg:tracking-spaced">
             Soluções e <br />
             oportunidades
           </span>
         </div>
 
-        <div className="SloganAndButtonContainer flex flex-col justify-between items-start mt-12 w-full h-40">
-          <p className="Slogan text-header-text text-2xl font-medium tracking-spaced">
+        <div className="SloganAndButtonContainer flex flex-col justify-between items-start mt-12 w-full reg:h-40 h-24">
+          <p className="Slogan text-header-text reg:text-2xl text-xl font-medium tracking-spaced">
             Para sua empresa crescer e prosperar.
           </p>
           <IntroButton />
         </div>
       </div>
 
-      <div className="ImageContainer ml-16">
+      <div className="ImageContainer col-span-2 ml-16">
         <Image src={ballsHehehe} alt="Image with blue balls lol" />
       </div>
     </div>
