7532503a05767462dd7a072b9d0766a1101b0da1
danielsampar <daniel.sampar12@gmail.com>
2023-09-12 17:50:43 -0300

    fix: change product cards text leading and size for smaller display

--- src/components/Cards/ProductCard/index.tsx
+++ src/components/Cards/ProductCard/index.tsx
@@ -16,10 +16,10 @@ export function ProductCard({
       <div className="IconContainer flex flex-row w-full justify-start reg:mb-6 mb-4">
         <Image src={imagePath} alt="Product image" />
       </div>
-      <span className="ProductTitle text-black font-inter reg:text-[2rem] text-2xl leading-none font-medium reg:mb-8 mb-4">
+      <span className="ProductTitle text-black font-inter reg:text-2xl text-lg font-medium reg:mb-8 mb-4">
         {title}
       </span>
-      <p className="ProductDescription text-black/60 font-inter reg:text-lg text-base leading-none font-normal">
+      <p className="ProductDescription text-black/60 font-inter reg:text-lg text-base font-normal">
         {description}
       </p>
     </div>
